11b1101837ee083d3446d45e0af5913da40780f8
Shraddha Zore <shraddhak1995@gmail.com>
2020-06-24 11:34:03 +0530

    BWA-1 : Skeleton Code

---
+++ src/main/java/com/shraddha/bookwebapp/BookWebappApplication.java
@@ -0,0 +1,13 @@
+package com.shraddha.bookwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shraddha/bookwebapp/BookWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shraddha.bookwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
